0cfb2258702380ecab3fc6ed01bf2d8f611cc660
Grim <grimonpont.jeremy@gmail.com>
2025-09-04 11:47:39 +0200

    chore(cleanup): untrack dist-tests/* and logs/*; enforce ignore via .gitignore

--- dist-tests/lib/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = log;
-function log(level, msg, fields) {
-    const base = { ts: new Date().toISOString(), level, msg };
-    console.log(JSON.stringify({ ...base, ...fields }));
-}
